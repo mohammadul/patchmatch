94dcba7b2eb51326c3628dd0a7bfc7a5a611bf9e
Sk. Mohammadul Haque <mohammadul@gmail.com>
2013-07-27 01:17:07 +0530

    Commit

--- testpatchmatch.m
+++ testpatchmatch.m
@@ -2,7 +2,7 @@
 clear; % make;
 clc; clear;
 disp('------------------------------------');
-img = double()imresize(imread('rlena.png'),0.25)); 
+img = double(imresize(imread('lena.png'),0.25)); 
 patchsize = 5; nn = 10;
 tic;
 cnn = patchmatch(img, [], patchsize, nn, 20, [], 1, [], [], [], 2);
